327fc639e0d77be21e5566ceb79b166f98362a48
Daisuke Nakahara <daisuken@industryofconservation.com>
2026-03-16 13:12:23 -0700

    Update GitHub Page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "313052779" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2051230519" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
